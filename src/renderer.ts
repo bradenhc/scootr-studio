1850c0bcd0a14c4056728b094b81ca2b5de13991
Braden Hitchcock <developer.hitchcock@gmail.com>
2021-06-07 06:15:23 -0600

    deps: add Preact and HTM

--- src/renderer.ts
+++ src/renderer.ts
@@ -12,34 +12,15 @@
 * specific language governing permissions and limitations under the License.                                           *
 *                                                                                                                      *
 **************************************************** END COPYRIGHT ****************************************************/
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import { h, render } from 'preact';
+import htm from 'htm';
 
 import './index.css';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const html = htm.bind(h);
+
+function App(props: any) {
+    return html`<h1>Hello ${props.name}!</h1>`;
+}
+
+render(html`<${App} name="World" />`, document.body);
